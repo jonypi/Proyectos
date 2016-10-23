7674edc53ec5a2a7c74d201c4ee1adcfb89d58ba
jonypi <jopillajo@udlanet.ec>
2016-10-23 18:00:29 -0500

    Version 1.0

--- PryVehiculo/src/Proyecto/Principal.java
+++ PryVehiculo/src/Proyecto/Principal.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pryvehiculo;
+package Proyecto;
 
 /**
  *
  * @author cisco
  */
-public class Vehiculo {
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
